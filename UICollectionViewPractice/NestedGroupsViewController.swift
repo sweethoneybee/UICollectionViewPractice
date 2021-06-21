6801af8654b9f0feb13dd839e2125c46ef8a8447
sweethoneybee <jsjphone8@gmail.com>
2021-06-21 22:25:34 +0900

    NestedGroups 적용
    그룹의 아이템 개수가 정해진 경우, vertical이냐 horizontal이냐에 따라
    height, width 가 자동으로 적용됨

---
+++ UICollectionViewPractice/NestedGroupsViewController.swift
@@ -0,0 +1,117 @@
+//
+//  NestedGroupsViewController.swift
+//  UICollectionViewPractice
+//
+//  Created by 정성훈 on 2021/06/21.
+//
+// from: https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/implementing_modern_collection_views
+
+import UIKit
+
+class NestedGroupsViewController: UIViewController {
+    
+    enum Section {
+        case main
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>!
+    var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Nested Groups"
+        configureHierarchy()
+        configureDataSource()
+    }
+
+}
+
+extension NestedGroupsViewController {
+
+    //   +-----------------------------------------------------+
+    //   | +---------------------------------+  +-----------+  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |     1     |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  +-----------+  |
+    //   | |               0                 |                 |
+    //   | |                                 |  +-----------+  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |     2     |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |           |  |
+    //   | +---------------------------------+  +-----------+  |
+    //   +-----------------------------------------------------+
+
+    /// - Tag: Nested
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout{
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let leadingItem = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.7),
+                                                   heightDimension: .fractionalHeight(1.0)))
+            leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            
+            let trailingItem = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(0.3)))
+            trailingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            let trailingGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3),
+                                                   heightDimension: .fractionalHeight(1.0)),
+                subitem: trailingItem, count: 2)
+            
+            let nestedGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(0.4)),
+                subitems: [leadingItem, trailingGroup])
+            let section = NSCollectionLayoutSection(group: nestedGroup)
+            return section
+            
+        }
+        return layout
+    }
+}
+
+extension NestedGroupsViewController {
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    
+    private func configureDataSource() {
+        
+        let cellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
+            cell.label.text = "\(indexPath.section), \(indexPath.item)"
+            cell.contentView.backgroundColor = .blue
+            cell.contentView.layer.borderColor = UIColor.black.cgColor
+            cell.contentView.layer.borderWidth = 1.0
+            cell.contentView.layer.cornerRadius = 8
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([Section.main])
+        snapshot.appendItems(Array(0..<100))
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
+
+extension NestedGroupsViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
